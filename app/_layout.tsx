22203c324e67fc821e4bed917e5ed79627f85ad0
serunio <jak.haraszkiewicz@gmail.com>
2026-03-24 17:28:42 +0100

    paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,32 @@
 import {Stack} from "expo-router";
 import {colors} from "@/styles";
 import {useFonts} from "expo-font";
+import {PaperProvider} from "react-native-paper";
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     'Sinacherib': require("./../assets/fonts/Sinacherib.ttf"),
     'Andika': require("./../assets/fonts/Andika/Andika-Regular.ttf")
   });
-  return <Stack
-    screenOptions={
-      {
-        headerStyle: {
-          backgroundColor: colors.background,
-        },
-        headerTintColor: colors.stroke,
-        headerTitleStyle: {
-          fontFamily: 'Andika'
-        },
-      }
-    }>
-    <Stack.Screen name={'index'} options={{title: 'Znaki'}}/>
-    <Stack.Screen name={'draw'} options={{title: 'Rysuj', headerBackButtonDisplayMode: 'minimal'}}/>
-  </Stack>;
+
+
+
+  return <PaperProvider>
+    <Stack
+      screenOptions={
+        {
+          headerStyle: {
+            backgroundColor: colors.background,
+          },
+          headerTintColor: colors.stroke,
+          headerTitleStyle: {
+            fontFamily: 'Andika'
+          },
+        }
+      }>
+      <Stack.Screen name={'index'} options={{title: 'Znaki'}}/>
+      <Stack.Screen name={'draw'}  options={{title: 'Rysuj', headerBackButtonDisplayMode: 'minimal'}}/>
+    </Stack>
+  </PaperProvider>
+
 }
