ac6c81c12e60f6baa7027ed66d42cdb7ec739ec3
serunio <jak.haraszkiewicz@gmail.com>
2026-03-24 17:29:22 +0100

    list separate page

--- app/index.tsx
+++ app/index.tsx
@@ -1,30 +1,10 @@
-import {Animated} from "react-native";
-import {api} from '@/api'
-import {useEffect, useState} from "react";
-import {Cunei} from '@/types'
-import CuneiListElement from "@/components/CuneiListElement";
-import ScrollView = Animated.ScrollView;
-import {colors} from "@/styles";
-
+import List from "@/app/list";
 
 export default function Index() {
-  const [cunei, setCunei] = useState<Cunei[]>([])
-  useEffect(() => {
-    fetchCunei()
-  }, [])
 
-  async function fetchCunei() {
-    try {
-      const response = await fetch(api + '/cunei')
-      const data = await response.json()
-      setCunei(data)
-    } catch(e) {
-      console.log(e)
-    }
-  }
-  return <ScrollView style={{backgroundColor: colors.background}}>
-    {cunei.map((c) => (
-      <CuneiListElement key={c.id} cunei={c}/>
-    ))}
-  </ScrollView>
+
+  return <>
+    <List />
+  </>;
 }
+
